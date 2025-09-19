b41749501d142e3c5b80497c073ec7d92a51e2aa
Pedro Laracuente <laracuentepedro@gmail.com>
2025-09-19 09:59:47 -0400

    add main script with text parser, and word count stats

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("greetings boots")
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+def count_words(text):
+    num_words = len(text.split())
+    print(f"{num_words} words found in the document")
+    return num_words
+def main():
+    book_text = get_book_text('./books/frankenstein.txt')
+    print(book_text)
+    count_words(book_text)
+
+main()
+
