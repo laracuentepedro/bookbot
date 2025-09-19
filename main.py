2f59757b8de94e18ae56a02abf20907893fa82ed
Pedro Laracuente <laracuentepedro@gmail.com>
2025-09-19 13:13:09 -0400

    feat: add command-line argument support for book file input

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 from stats import count_words, count_chars, create_report
+import sys
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
 def main():
-    book_text = get_book_text('./books/frankenstein.txt')
+    if len(sys.argv) < 2:
+        print("Looks like you're missing an argument!")
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_text = get_book_text(sys.argv[1])
     print(book_text)
     words = count_words(book_text)
     chars = count_chars(book_text)
     print(chars)
     report = create_report(chars)
-    print("============ BOOKBOT ============\nAnalyzing book found at books/frankenstein.txt...\n----------- Word Count -----------")
+    print(f"============ BOOKBOT ============\nAnalyzing book found at {sys.argv[1]}...\n----------- Word Count -----------")
     print(f"Found {words} total words")
     print("--------- Character Count -------")
     for entry in report:
