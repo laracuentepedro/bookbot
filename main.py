94267391789ef752d7bb5d73b7e2d0876704ff25
Pedro Laracuente <laracuentepedro@gmail.com>
2025-09-19 10:16:22 -0400

    refactor: move count_words function to stats.py and update main.py

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
+from stats import count_words
+
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
-def count_words(text):
-    num_words = len(text.split())
-    print(f"{num_words} words found in the document")
-    return num_words
 def main():
     book_text = get_book_text('./books/frankenstein.txt')
     print(book_text)
---
+++ stats.py
@@ -0,0 +1,4 @@
+def count_words(text):
+    num_words = len(text.split())
+    print(f"{num_words} words found in the document")
+    return num_words
